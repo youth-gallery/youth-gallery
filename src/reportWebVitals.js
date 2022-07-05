ee68c9e20ce1960556f504dcdd1875afe2fe757e
areumz <ksah1016@naver.com>
2022-07-05 16:28:10 +0900

    Feat: 기본 환경 세팅

---
+++ src/reportWebVitals.js
@@ -0,0 +1,17 @@
+//리액트 성능 측정시 필요
+
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import('web-vitals').then(
+            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+                getCLS(onPerfEntry);
+                getFID(onPerfEntry);
+                getFCP(onPerfEntry);
+                getLCP(onPerfEntry);
+                getTTFB(onPerfEntry);
+            }
+        );
+    }
+};
+
+export default reportWebVitals;

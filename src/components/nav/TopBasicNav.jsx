bb79ad03bae19571730f870979db4a0b4b5873ba
minkyeongJ <jomk2221@naver.com>
2022-07-09 09:45:17 +0900

    DesignCSS(TopBasicNav.jsx): 스타일드 컴포넌트 세분화하여 태그별 스타일 분리 적용

--- src/components/nav/TopBasicNav.jsx
+++ src/components/nav/TopBasicNav.jsx
@@ -3,12 +3,39 @@ import React from 'react';
 import iconArrowLeft from '../../assets/icon-arrow-left.png';
 import logoutDot from '../../assets/icon-more-vertical.png';
 
+const NavStyle = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12px 12px 12px 13px;
+`;
+
+const NavButton = styled.button`
+    //reset
+    border: initial;
+    background-color: initial;
+`;
+
+const ArrowLeftButton = styled(NavButton)`
+    width: 22px;
+    height: 22px;
+    background-image: url(${iconArrowLeft});
+    background-size: cover;
+`;
+
+const LogoutButton = styled(NavButton)`
+    width: 24px;
+    height: 24px;
+    background-image: url(${logoutDot});
+    background-size: cover;
+`;
+
 function TopBasicNav() {
     return (
         <>
             <NavStyle className="nav">
-                <button className="arrow-left"></button>
-                <button className="btn-logout"></button>
+                <ArrowLeftButton />
+                <LogoutButton />
             </NavStyle>
         </>
     );
@@ -16,27 +43,4 @@ function TopBasicNav() {
 
 export default TopBasicNav;
 
-const NavStyle = styled.section`
-    //btn reset
-    button {
-        border: initial;
-        background-color: initial;
-    }
-    //
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 12px 12px 12px 13px;
-    .arrow-left {
-        width: 22px;
-        height: 22px;
-        background-image: url(${iconArrowLeft});
-        background-size: cover;
-    }
-    .btn-logout {
-        width: 24px;
-        height: 24px;
-        background-image: url(${logoutDot});
-        background-size: cover;
-    }
-`;
+

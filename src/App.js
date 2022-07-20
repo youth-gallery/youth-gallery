d9c7f80e93d74ac8c6daef8bd68113d626d2d4d6
minkyeongJ <jomk2221@naver.com>
2022-07-21 03:15:06 +0900

    Refactor: 앞으로 사용 할 포맷으로 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles/reset.css';
 import './styles/global.css';
-import Login from './components/login/Login';
 import { BrowserRouter } from 'react-router-dom';
+import PageRouter from './pages/PageRouter';
 
 function App() {
     return (
         <BrowserRouter>
-            <Login />
+            <PageRouter />
         </BrowserRouter>
     );
 }

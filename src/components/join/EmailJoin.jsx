7d34a397dc5e452d4128ce6eface61ed11409e49
Soohyeon Kim <tngusglaso@gmail.com>
2022-07-24 03:52:28 +0900

    Fix: 로그인 페이지 위해 컴포넌트를 input박스로 분리 로그인 기능구현을 위해 모듈화 해놓았던 컴포넌트를 다시 input박스로 분리하였습니다.

--- src/components/join/EmailJoin.jsx
+++ src/components/join/EmailJoin.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './EmailJoin.module.css';
-import LoginInput from '../LoginInput';
 import Title from '../login/Title';
 import NextDisalbedButton from '../../components/button/NextDisabledButton';
 
@@ -10,15 +9,23 @@ function EmailJoin() {
             <section className={styles.emailJoin_body}>
                 <Title title={'이메일로 회원가입'} />
                 <div className={styles.emailJoin_title}>
-                    <LoginInput
-                        title={'이메일'}
-                        placeholder={'이메일 주소를 입력해주세요.'}
-                        type={'email'}
+                    <label htmlFor="input_id">이메일</label>
+                    <input
+                        id="input_id"
+                        type="email"
+                        placeholder="이메일 주소를 입력해주세요."
+                        onChange={(e) => {
+                            e.target.value;
+                        }}
                     />
-                    <LoginInput
-                        title={'비밀번호'}
-                        placeholder={'비밀번호를 입력해주세요.'}
-                        type={'password'}
+                    <label htmlFor="input_pw">비밀번호</label>
+                    <input
+                        id="input_pw"
+                        type="password"
+                        placeholder="비밀번호를 입력해주세요."
+                        onChange={(e) => {
+                            e.target.value;
+                        }}
                     />
                 </div>
                 <NextDisalbedButton />
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import styles from './Login.module.css';
-import LoginInput from '../LoginInput';
+// import LoginInput from '../LoginInput';
 import Title from './Title';
 import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+// import axios from 'axios';
 
 function Login() {
+    // const [loginId, setLoginId] = useState('');
+    // const [loginPw, setLoginPw] = useState('');
+
     return (
         <>
             <section className={styles.login_body}>
                 <Title title={'로그인'} />
                 <div className={styles.login_title}>
-                    <LoginInput title={'이메일'} type={'email'} />
-                    <LoginInput title={'비밀번호'} type={'password'} />
+                    <label htmlFor="input_id">이메일</label>
+                    <input
+                        id="input_id"
+                        type="email"
+                        onChange={(e) => {
+                            e.target.value;
+                        }}
+                    />
+                    <label htmlFor="input_pw">비밀번호</label>
+                    <input
+                        id="input_pw"
+                        type="password"
+                        onChange={(e) => {
+                            e.target.value;
+                        }}
+                    />
                 </div>
                 <button className={styles.login_btn}>
                     <span className={styles.login_span}>로그인</span>

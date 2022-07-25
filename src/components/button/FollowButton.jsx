f1da2ca76096e19778d99471b61be2c6d7fd53d3
sabit1997 <yejiii1997@gmail.com>
2022-07-25 20:54:40 +0900

    Feat: 팔로우 기능 추가

--- src/components/button/FollowButton.jsx
+++ src/components/button/FollowButton.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -16,9 +17,31 @@ const Span = styled.span`
 `;
 
 function FollowButton() {
+    const follow = () => {
+        // 0004 임시토큰
+        const token =
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZDg1MzkxMTdhZTY2NjU4MTdlNjY3MyIsImV4cCI6MTY2MzkzMzg1NSwiaWF0IjoxNjU4NzQ5ODU1fQ.pDSDuGNU51d1C8TI2_-wcADNOSqKkf_lJL3oMBB0clo';
+        axios
+            .post(
+                'https://mandarin.api.weniv.co.kr/profile/0002/follow',
+                {},
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-type': 'application/json',
+                    },
+                }
+            )
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
     return (
         <>
-            <FollowButtonBody>
+            <FollowButtonBody onClick={follow}>
                 <Span>팔로우</Span>
             </FollowButtonBody>
         </>

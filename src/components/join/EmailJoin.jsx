47a98d92bb218329932e2c3e0c9763017e1ace67
Soohyeon Kim <tngusglaso@gmail.com>
2022-07-27 20:49:00 +0900

    Feat: 회원가입 유효성검사 통과시 프로필설정으로 이동

--- src/components/join/EmailJoin.jsx
+++ src/components/join/EmailJoin.jsx
@@ -4,6 +4,7 @@ import Title from '../login/Title';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function EmailJoin() {
     const [joinId, setJoinId] = useState('');
@@ -13,19 +14,21 @@ function EmailJoin() {
     const [joinEmailConfirm, setJoinEmailConfirm] = useState(false);
     const [joinPwConfirm, setJoinPwConfirm] = useState(false);
     const [msg, setMsg] = useState('');
+    const navigate = useNavigate();
 
     const handleJoinId = (e) => {
         setJoinId(e.target.value);
     };
 
     const handleJoinPw = (e) => {
-        setJoinPw(e.target.value);
+        const joinPw = e.target.value;
+        setJoinPw(joinPw);
 
         console.log(`joinPw ${joinPw}`);
         console.log(`joinPw.length ${joinPw.length}`);
 
-        // 비밀번호 유효성검사 (6자리 이상일 때만 통과) // TODO: 처음 쳤을 때 바로 1로 반영 안되는 이슈! 해결하기!!
-        joinPw.length >= 5 ? setJoinPwConfirm(true) : setJoinPwConfirm(false);
+        // 비밀번호 유효성검사 (6자리 이상일 때만 통과)
+        joinPw.length >= 6 ? setJoinPwConfirm(true) : setJoinPwConfirm(false);
     };
 
     // 포커스 잃으면 유효성검사 진행 => TODO: 첫로딩시 경고문구 지워주기!
@@ -35,9 +38,6 @@ function EmailJoin() {
             /* eslint-disable-next-line */
             /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
         emailRegex.test(joinId) ? setIsEmail(true) : setIsEmail(false);
-
-        // // 비밀번호 유효성검사 (6자리 이상일 때만 통과)
-        // joinPw.length >= 6 ? setJoinPwConfirm(true) : setJoinPwConfirm(false);
 
         console.log(`isEmail ${isEmail}`);
         console.log(`joinPwConfirm ${joinPwConfirm}`);
@@ -62,7 +62,6 @@ function EmailJoin() {
 
             if (res.data.message === '사용 가능한 이메일 입니다.') {
                 setJoinEmailConfirm(true);
-                setMsg(`*${res.data.message}`);
             } else if (res.data.message === '이미 가입된 이메일 주소 입니다.') {
                 setJoinEmailConfirm(false);
                 setMsg(`*${res.data.message}`);
@@ -73,34 +72,6 @@ function EmailJoin() {
             console.log(error);
         }
     };
-
-    // // 다음 버튼 클릭시
-    // // 이메일 중복검사 => 미통과시 경고문구 출력!
-    // const onClickJoin = async () => {
-    //     try {
-    //         const res = await axios.post(
-    //             'https://mandarin.api.weniv.co.kr/user/emailvalid/',
-    //             {
-    //                 user: {
-    //                     email: joinId,
-    //                 },
-    //             }
-    //         );
-    //         console.log(res);
-
-    //         if (res.data.message === '사용 가능한 이메일 입니다.') {
-    //             setJoinEmailConfirm(true);
-    //             setMsg(`*${res.data.message}`);
-    //         } else if (res.data.message === '이미 가입된 이메일 주소 입니다.') {
-    //             setJoinEmailConfirm(false);
-    //             setMsg(`*${res.data.message}`);
-    //         } else {
-    //             setJoinEmailConfirm(false);
-    //         }
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
 
     useEffect(() => {
         // 이메일 유효성검사, 중복검사, 비밀번호 유효성검사 모두 통과 => 다음 버튼 활성화
@@ -113,7 +84,7 @@ function EmailJoin() {
             }`
         );
         setIsActive(isEmail && joinEmailConfirm && joinPwConfirm);
-
+        navigate('/membership');
         return () => {};
     }, [joinId, joinPw]);
 

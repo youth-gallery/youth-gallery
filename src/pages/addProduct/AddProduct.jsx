bc6caeebb226e1d06a9037c9e4ce0a6bf2fc0e33
minkyeongJ <jomk2221@naver.com>
2022-07-30 04:23:01 +0900

    DesignCSS: 상품등록 배경 수정

--- src/pages/addProduct/AddProduct.jsx
+++ src/pages/addProduct/AddProduct.jsx
@@ -114,75 +114,77 @@ const AddProduct = () => {
                 <TopUploadNav title="저장" state={state} />
             </Nav>
             <section className={styles.input_section}>
-                <h2 className={styles.add_image_title}>이미지 등록</h2>
-                {
-                    <section
-                        className={styles.upload_img_section}
-                        style={backgroundstyle}
+                <div className={styles.input_inner_section}>
+                    <h2 className={styles.add_image_title}>이미지 등록</h2>
+                    {
+                        <section
+                            className={styles.upload_img_section}
+                            style={backgroundstyle}
+                        >
+                            <h3 className={styles.ir}>
+                                업로드할 이미지가 보이는 곳 입니다.
+                            </h3>
+                            <label
+                                className={styles.image_upload_button}
+                                htmlFor={styles.image_upload_input}
+                            ></label>
+                            <input
+                                id={styles.image_upload_input}
+                                type="file"
+                                accept="image/*"
+                                ref={inpRef}
+                                onChange={handleImageInput}
+                                onClick={onPicBtnClick}
+                            ></input>
+                        </section>
+                    }
+                    <label
+                        className={styles.input_title}
+                        htmlFor={styles.product_name_input}
                     >
-                        <h3 className={styles.ir}>
-                            업로드할 이미지가 보이는 곳 입니다.
-                        </h3>
-                        <label
-                            className={styles.image_upload_button}
-                            htmlFor={styles.image_upload_input}
-                        ></label>
-                        <input
-                            id={styles.image_upload_input}
-                            type="file"
-                            accept="image/*"
-                            ref={inpRef}
-                            onChange={handleImageInput}
-                            onClick={onPicBtnClick}
-                        ></input>
-                    </section>
-                }
-                <label
-                    className={styles.input_title}
-                    htmlFor={styles.product_name_input}
-                >
-                    상품명
-                </label>
-                <input
-                    id={styles.product_name_input}
-                    className={styles.product_input}
-                    type="text"
-                    placeholder="2~15자 이내여야 합니다."
-                    onChange={(e) => {
-                        setName(e.target.value);
-                    }}
-                />
-                <label
-                    className={styles.input_title}
-                    htmlFor={styles.product_price_input}
-                >
-                    가격
-                </label>
-                <input
-                    id={styles.product_price_input}
-                    className={styles.product_input}
-                    type="text"
-                    value={price}
-                    placeholder="숫자만 입력 가능합니다."
-                    onChange={(e) => {
-                        setPrice(inputPriceFormat(e.target.value));
-                    }}
-                />
-                <label
-                    className={styles.input_title}
-                    htmlFor={styles.product_link_input}
-                >
-                    판매링크
-                </label>
-                <input
-                    id={styles.product_link_input}
-                    className={styles.product_input}
-                    type="text"
-                    placeholder="URL을 입력해 주세요."
-                    onChange={(e) => {
-                        setLink(e.target.value);
-                    }}
-                />
+                        상품명
+                    </label>
+                    <input
+                        id={styles.product_name_input}
+                        className={styles.product_input}
+                        type="text"
+                        placeholder="2~15자 이내여야 합니다."
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
+                    <label
+                        className={styles.input_title}
+                        htmlFor={styles.product_price_input}
+                    >
+                        가격
+                    </label>
+                    <input
+                        id={styles.product_price_input}
+                        className={styles.product_input}
+                        type="text"
+                        value={price}
+                        placeholder="숫자만 입력 가능합니다."
+                        onChange={(e) => {
+                            setPrice(inputPriceFormat(e.target.value));
+                        }}
+                    />
+                    <label
+                        className={styles.input_title}
+                        htmlFor={styles.product_link_input}
+                    >
+                        판매링크
+                    </label>
+                    <input
+                        id={styles.product_link_input}
+                        className={styles.product_input}
+                        type="text"
+                        placeholder="URL을 입력해 주세요."
+                        onChange={(e) => {
+                            setLink(e.target.value);
+                        }}
+                    />
+                </div>
             </section>
         </form>
     );
